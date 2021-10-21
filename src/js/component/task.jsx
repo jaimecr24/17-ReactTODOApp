f7865389f3515b18bebbf282d58660bd2f365e53
Jaime <jaimecr72@gmail.com>
2021-10-21 18:32:59 +0000

    added visibility/inv of 'X'

--- src/js/component/task.jsx
+++ src/js/component/task.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 const Task = props => {
+	// if bVisible is true the 'X' is visible
+	const [bVisible, setVisible] = useState(false);
+
 	const handleClickX = () => {
 		// when user press 'X' we update the list of task removing the task pressed
 		// filter returns all the items of list except who has i == props.index. (index of this task)
@@ -9,9 +12,14 @@ const Task = props => {
 	};
 
 	return (
-		<div className="row">
+		<div
+			className="row"
+			onMouseOver={() => setVisible(true)}
+			onMouseLeave={() => setVisible(false)}>
 			<div className="col-auto me-auto">{props.list[props.index]}</div>
-			<div className="col-auto" onClick={handleClickX}>
+			<div
+				className={`col-auto ${bVisible ? "visible" : "invisible"}`}
+				onClick={handleClickX}>
 				<a href="#">X</a>
 			</div>
 		</div>
